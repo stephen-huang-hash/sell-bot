5d418c901cfb6dd9a47671540c8d35bb90cc1426
Stephen Huang <huangstephen9@gmail.com>
2024-06-02 01:48:21 -0400

    initial commit

--- images.js
+++ images.js
@@ -1,8 +1,8 @@
 const images = {
     "Toad": "https://tr.rbxcdn.com/28bae58f2c0ac2767fa80d2ac4e45889/150/150/Hat/Webp",
-    "Splatoon Hat": "https://tr.rbxcdn.com/47b6113a1f12caa698b5709289abdc4e/150/150/Hat/Webp",
-    "Splatoon Hat (Blue)": "https://tr.rbxcdn.com/31cea1cbce404c5fdfcf8168fb0c012c/150/150/Hat/Webp",
-    "Splatoon Hat (Red)": "https://tr.rbxcdn.com/eabef1119b2f8067f2f801d815c545ae/150/150/Hat/Webp",
+    "Orange Squid": "https://tr.rbxcdn.com/47b6113a1f12caa698b5709289abdc4e/150/150/Hat/Webp",
+    "Blue Squid": "https://tr.rbxcdn.com/31cea1cbce404c5fdfcf8168fb0c012c/150/150/Hat/Webp",
+    "Red Squid": "https://tr.rbxcdn.com/eabef1119b2f8067f2f801d815c545ae/150/150/Hat/Webp",
     "Riku": "https://tr.rbxcdn.com/876e28a558debbd445e6d0e434bece4c/150/150/Hat/Webp",
     "Ayanokoji": "https://tr.rbxcdn.com/55acfb8063bc51d10482b8f3403b0147/150/150/Hat/Webp",
     "Kung Fu Panda": "https://tr.rbxcdn.com/843322e8e3b2d54684e053aa804c978f/150/150/Hat/Webp",
@@ -12,12 +12,17 @@ const images = {
     "Garfield": "https://tr.rbxcdn.com/eb5fb2ff4f8ced8c8a94f2bcfffe7ebf/150/150/Hat/Webp",
     "Sonic (Movie)": "https://tr.rbxcdn.com/1c61ad89c16aa8b0d50036a8b7e097f7/150/150/Hat/Webp",
     "Toad (Green)": "https://tr.rbxcdn.com/ec025797a8da6f12019150b7052b8192/150/150/Hat/Webp",
-    "Deku Hair": "https://tr.rbxcdn.com/25e448505c7936c74796c46a40234404/150/150/Hat/Webp",
+    "Deku": "https://tr.rbxcdn.com/25e448505c7936c74796c46a40234404/150/150/Hat/Webp",
     "Immortal Undead Sword": "https://tr.rbxcdn.com/bdb6cf32498107ed4f51f45f0bcfcdd2/150/150/Hat/Webp",
     "Messy Red Anime Hair": "https://tr.rbxcdn.com/efbef76182502002ec22be5da0b9bb52/150/150/Hat/Webp",
-    "Blonde Anime Hair": "https://tr.rbxcdn.com/2fc21e8d2433da0ee4856fa76f79c1cc/150/150/Hat/Webp",
+    "Elaine": "https://tr.rbxcdn.com/2fc21e8d2433da0ee4856fa76f79c1cc/150/150/Hat/Webp",
     "Susanoo": "https://tr.rbxcdn.com/a54093dabc61149f57959525a00daeb5/150/150/Hat/Webp",
-    "Mario Kart": "https://tr.rbxcdn.com/8c4f865936200bd65815340ffe935ccc/150/150/Hat/Webp"
+    "Mario Kart": "https://tr.rbxcdn.com/8c4f865936200bd65815340ffe935ccc/150/150/Hat/Webp",
+    "King Julien": "https://tr.rbxcdn.com/0bf1f0e68069de88be3ba2e332f440ad/150/150/Hat/Webp",
+    "Gengar Backpack": "https://tr.rbxcdn.com/f5419e681d063f78b59f1eedd8be224c/150/150/Hat/Webp",
+    "Cursed Koopa": "https://tr.rbxcdn.com/2e1eff0ef0f7604325d0c2416957d5ce/150/150/Hat/Webp",
+    "Armored Titan": "https://tr.rbxcdn.com/25651c5d79ff78d4e51d68861fb01f36/150/150/Hat/Webp",
+    "Yoshi": "https://tr.rbxcdn.com/6cb627025039d64cef892200a1bf1d1d/150/150/Hat/Webp",
 }
 
 module.exports = images;
